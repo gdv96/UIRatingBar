3678334bef97e0c5b9c3887772b681345d3549ca
Giovanni De Vanno <giovannidv96@gmail.com>
2022-12-06 12:32:52 +0100

    implementato la classe RatingBarActivity

--- app/src/main/java/com/unisannio/gdevanno/uiratingbar/RatingBarActivity.java
+++ app/src/main/java/com/unisannio/gdevanno/uiratingbar/RatingBarActivity.java
@@ -3,6 +3,8 @@ package com.unisannio.gdevanno.uiratingbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class RatingBarActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class RatingBarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating_bar);
+
+        final TextView tv = (TextView) findViewById(R.id.textView);
+        final RatingBar bar = (RatingBar) findViewById(R.id.ratingbar);
+
+        bar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+
+            //Called when the user swipes the RatingBar
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                tv.setText("Rating:" + v);
+
+            }
+        });
     }
 }
